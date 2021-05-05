8a99ad797cff825ef2089f9960eb5bcccc14823a
Pony Smith <ponysmith@users.noreply.github.com>
2021-05-05 15:56:25 -0700

    Adding root response for heartbeat; cleanup

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,5 @@
 import knex from 'knex';
 import knexfile from '../knexfile';
 
-// const environment = process.env.NODE_ENV || 'development';
 const config = knexfile[process.env.NODE_ENV];
 export default knex(config);
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,13 @@ import Router_v1 from './v1';
 
 const RootRouter = new Router();
 
+RootRouter.get('/', async(ctx, next) => {
+  ctx.body = {
+    "status": 1,
+    "env": process.env.NODE_ENV
+  }
+});
+
 RootRouter
   .use(Router_v1.routes())
   .use(Router_v1.allowedMethods())
